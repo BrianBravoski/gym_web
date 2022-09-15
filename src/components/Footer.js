c3e9019174efab498ed9ae790551667dc365c990
BrianBravoski <brianbravoski28@gmail.com>
2022-09-15 09:19:21 +0300

    finishing footer and exercise details

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,8 +1,20 @@
-import React from 'react'
+import React from 'react';
+import { Stack, Box, typography } from '@mui/system';
+
+import Logo from '../assets/images/Logo-1.png'
+import { Typography } from '@mui/material';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <Box mt="80px" bgcolor="#fff3f4">
+      <Stack gap="40px" alignItems="center" px="40px" pt="24px">
+        <img src={Logo} alt="logo" width="200px" height="40px" />
+        <Typography variant='h5' pb='40px' mt='10px'>
+          Made in React ❤ by BrianBravoski
+        </Typography>
+      </Stack>
+
+    </Box>
   )
 }
 
--- src/components/SimilarExercises.js
+++ src/components/SimilarExercises.js
@@ -1,8 +1,25 @@
 import React from 'react'
+import { Box, Stack, Typography } from '@mui/material'
 
-const SimilarExercises = () => {
+import HorizontalScrollbar from './HorizontalScrollBar'
+import Loader from './Loader'
+
+const SimilarExercises = ({ targetMuscleExercises, equipmentExercises }) => {
   return (
-    <div>SimilarExercises</div>
+    <Box sx={{ mt: { lg: '100px', xs: '0px' } }}>
+      <Typography sx={{ fontSize:{lg:'44px', xs:'25px'}, ml:'20px'}} fontWeight={700} color="#000" mb="33px">
+       Similar  <span style={{ color:"#ff2625", textTransform:'capitalize'}}>Target Muscle </span> Exercise
+      </Typography>
+      <Stack direction="row" sx={{ p: '2', position: 'relative' }}>
+        {targetMuscleExercises.length !== 0 ? <HorizontalScrollbar data={targetMuscleExercises}/> : <Loader/>}
+      </Stack>
+      <Typography sx={{ fontSize:{lg:'44px', xs:'25px'}, ml:'20px'}} fontWeight={700} color="#000" mb="33px">
+        Similar <span style={{ color:"#ff2625", textTransform:'capitalize'}}> Equipment </span> Exercises
+      </Typography>
+      <Stack direction="row" sx={{ p: '2', position: 'relative' }}>
+        {equipmentExercises.length ? <HorizontalScrollbar data={equipmentExercises}/> : <Loader/>}
+      </Stack>
+    </Box>
   )
 }
 
